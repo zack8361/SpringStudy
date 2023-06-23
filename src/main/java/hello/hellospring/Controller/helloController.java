fc83296053adc64c1ed5a7a65a0092fdf617abe2
zack8361 <zack6353@naver.com>
2023-06-23 20:52:18 +0900

    Spring Study 2일차
    Spring Study 2일차

--- src/main/java/hello/hellospring/Controller/helloController.java
+++ src/main/java/hello/hellospring/Controller/helloController.java
@@ -4,6 +4,8 @@ package hello.hellospring.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class helloController {
@@ -17,4 +19,20 @@ public class helloController {
        return "hello";
     }
 
+    @GetMapping("/hello-mvc")
+    public String helloMvc(@RequestParam("name") String name,Model model){
+        model.addAttribute("name", name);
+//        hello-template 를 찾아서 렌더링 시켜라.
+        return "hello-template";
+    }
+
+    @GetMapping("/hello-string")
+    @ResponseBody
+//    http protocol body 부에 내가 직접 데이터를 넣어주겠다.
+//    그대로 데이터만 내려주는 방식.
+//    크게 쓰이는 방식은 아니다..
+    public String helloString(@RequestParam("name") String name){
+        
+        return "hello" + name;
+    }
 }
