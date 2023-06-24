41750dea5439f22107f47d210608543efb8e334f
zack8361 <zack6353@naver.com>
2023-06-24 19:05:04 +0900

    Spring Study 3일차(2)
    Spring Study 3일차(2)

---
+++ src/main/java/hello/hellospring/Service/MemberService.java
@@ -0,0 +1,34 @@
+package hello.hellospring.Service;
+
+import hello.hellospring.Repository.MemberRepository;
+import hello.hellospring.Repository.MemoryMemberRepository;
+import hello.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    public Long join (Member member){
+        // 중복 회원 처리
+        duplicatedName(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void duplicatedName(Member member) {
+        memberRepository.findByName(member.getName())
+                        .ifPresent(m-> {
+                            throw new IllegalStateException("이미 존재하는 회원입니다.");
+                        });
+    }
+
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+
+}
--- src/test/java/hello/hellospring/Repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hellospring/Repository/MemoryMemberRepositoryTest.java
@@ -61,5 +61,5 @@ class MemoryMemberRepositoryTest {
         List<Member> result = repository.findAll();
         assertThat(result.size()).isEqualTo(2);
     }
-    
+
 }
