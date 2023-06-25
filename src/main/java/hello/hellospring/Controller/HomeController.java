5d1f9923be8cdae6b36fd80bd1be42985f32b7fb
zack8361 <zack6353@naver.com>
2023-06-25 18:16:35 +0900

    Spring Study 4일차(1)
    Spring Study 4일차(1)

---
+++ src/main/java/hello/hellospring/Controller/HomeController.java
@@ -0,0 +1,16 @@
+package hello.hellospring.Controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        System.out.println("@@@@@@@@@@@@ home.html 실행 @@@@@@@@@@@@@@@");
+        return "home";
+    }
+    
+}
--- src/main/java/hello/hellospring/Controller/MemberController.java
+++ src/main/java/hello/hellospring/Controller/MemberController.java
@@ -2,8 +2,18 @@ package hello.hellospring.Controller;
 
 
 import hello.hellospring.Service.MemberService;
+import hello.hellospring.domain.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
 
 
 /**
@@ -16,6 +26,32 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+
+    @GetMapping("/members/new")
+    public String createForm(){
+
+        return "members/createMemberForm";
+    }
+
+
+    @PostMapping("/members/new")
+    public String create(MemberForm memberForm){
+
+        Member member = new Member();
+        member.setName(memberForm.getName());
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+
+        return "members/memberList";
     }
     
 }
